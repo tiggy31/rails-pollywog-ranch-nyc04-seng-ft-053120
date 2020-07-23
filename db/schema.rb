90d244bd7bd347dfcc266cd1d3c753a5488cb24c
tiggy31 <tigist_alemu@fitnyc.edu>
2020-07-23 16:17:33 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 20140922151507) do
   end
 
   create_table "ponds", force: :cascade do |t|
-    t.string "water_type"
     t.string "name"
+    t.string "water_type"
   end
 
   create_table "tadpoles", force: :cascade do |t|
